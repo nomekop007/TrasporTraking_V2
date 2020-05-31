80484010609a08995d79b9537edf109a1bac64ec
diego <d.riosrojas007@gmail.com>
2020-05-31 16:47:26 -0400

    problema con archivo babel.config resuelto

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,6 @@
-/* module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
   };
-};
- */
-
-// babel.config.js
-module.exports = {
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        targets: {
-          node: "current",
-        },
-      },
-    ],
-  ],
 };
