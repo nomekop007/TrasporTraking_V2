8abffc4f4c7a0b7b1116bd006a13e32acc65d196
diego <d.riosrojas007@gmail.com>
2020-05-31 15:45:43 -0400

    primera separacion de logica de negocios entre renderTransport y transporte

--- app/components/Map/RenderTrasports.js
+++ app/components/Map/RenderTrasports.js
@@ -4,9 +4,9 @@ import { Marker, Callout } from "react-native-maps";
 import { firebaseapp } from "../../utils/Firebase";
 import firebase from "firebase/app";
 import "firebase/database";
-import "firebase/firestore";
 const database = firebase.database(firebaseapp);
-const firestore = firebase.firestore(firebaseapp);
+import { Transporte } from "../../model/Transporte";
+const trasporte = new Transporte();
 
 export default function renderMarkers(props) {
   const { UserLogged, toastRef } = props;
@@ -66,40 +66,10 @@ function LoadingInfoTrasport(props) {
   });
 
   useEffect(() => {
-    firestore
-      .collection("Transporte")
-      .doc(ID)
-      .get()
-      .then((doc) => {
-        const data = doc.data();
-
-        /* se cambia nombre de linea de trasporte */
-        switch (data.lineaTrasporte) {
-          case "l1":
-            data.lineaTrasporte = "linea A";
-            break;
-          case "l2":
-            data.lineaTrasporte = "linea B";
-            break;
-          case "l3":
-            data.lineaTrasporte = "linea C";
-            break;
-          case "l4":
-            data.lineaTrasporte = "linea D";
-            break;
-          case "l5":
-            data.lineaTrasporte = "linea Colin";
-            break;
-
-          default:
-            data.lineaTrasporte = "linea desconocida";
-            break;
-        }
-        setTrasport(data);
-      })
-      .catch(function (error) {
-        console.log("error de extraccion :", error);
-      });
+    const promesa = trasporte.buscarTransporte(ID);
+    promesa.then((trasporte) => {
+      setTrasport(trasporte);
+    });
   }, []);
 
   return (
--- app/model/Transporte.js
+++ app/model/Transporte.js
@@ -1,3 +1,8 @@
+import { firebaseapp } from "../utils/Firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+const firestore = firebase.firestore(firebaseapp);
+
 export class Transporte {
   idTransporte = String;
   nombreConductor = String;
@@ -10,5 +15,45 @@ export class Transporte {
     this.patente = patente;
     this.lineaTransporte = lineaTransporte;
     this.estado = estado;
+  }
+
+  buscarTransporte(idTransporte) {
+    let promesa = Promise.resolve(
+      firestore
+        .collection("Transporte")
+        .doc(idTransporte)
+        .get()
+        .then((doc) => {
+          const data = doc.data();
+          /* se cambia nombre de linea de trasporte */
+          switch (data.lineaTrasporte) {
+            case "l1":
+              data.lineaTrasporte = "linea A";
+              break;
+            case "l2":
+              data.lineaTrasporte = "linea B";
+              break;
+            case "l3":
+              data.lineaTrasporte = "linea C";
+              break;
+            case "l4":
+              data.lineaTrasporte = "linea D";
+              break;
+            case "l5":
+              data.lineaTrasporte = "linea Colin";
+              break;
+
+            default:
+              data.lineaTrasporte = "linea desconocida";
+              break;
+          }
+          return data;
+        })
+        .catch(function (error) {
+          console.log("error de extraccion :", error);
+        })
+    );
+
+    return promesa;
   }
 }
